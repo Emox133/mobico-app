d10c1d33741e8e85e6473a14f8d57e6a65b0aeac
Emox133 <emir.betaacc0unt@gmail.com>
2020-05-20 00:20:09 +0200

    Updates

---
+++ precache-manifest.5fae76cbc74a8d77b95d235d988df162.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f2e53a4b75f8c0c1f324a515dad2b3",
+    "url": "/mobico-app/index.html"
+  },
+  {
+    "revision": "bcdb42745996076c9efe",
+    "url": "/mobico-app/static/css/main.848ff8e3.chunk.css"
+  },
+  {
+    "revision": "8362958b72159b9809f2",
+    "url": "/mobico-app/static/js/2.20c50faa.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/mobico-app/static/js/2.20c50faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdb42745996076c9efe",
+    "url": "/mobico-app/static/js/main.249b2f2a.chunk.js"
+  },
+  {
+    "revision": "3ba92dff07681254f419",
+    "url": "/mobico-app/static/js/runtime-main.5d119695.js"
+  },
+  {
+    "revision": "8bc0caeee7617daa01408d63b3f97fc5",
+    "url": "/mobico-app/static/media/6.8bc0caee.jpg"
+  },
+  {
+    "revision": "e6d997b0437336e7dfa5912d026bc935",
+    "url": "/mobico-app/static/media/logo-mobico-5.e6d997b0.png"
+  }
+]);
