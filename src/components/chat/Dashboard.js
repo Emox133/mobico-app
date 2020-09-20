5dab1312f419dc20cd9f99b18d1930f89b886cfe
Emox133 <emir.betaacc0unt@gmail.com>
2020-09-20 17:10:57 +0200

    Quick fix for chat api

--- src/components/chat/Dashboard.js
+++ src/components/chat/Dashboard.js
@@ -7,20 +7,23 @@ import OpenConversation from './OpenConversation'
 
 import {useConversations} from '../../contexts/ConversationsProvider'
 import {useContacts} from '../../contexts/ContactsProvider'
+import {useMediaQuery} from '@material-ui/core'
+
 
 const Dashboard = (props) => {
     const {contacts} = useContacts()
     const {conversations, selectedConversation} = useConversations()
-
+    const isActive = useMediaQuery('(max-width: 600px)')
+    
     // console.log('Contacts:', contacts)
     // console.log('Conversations:', conversations)
     return (
-        <Grid container>
+        <Grid container style={{width: '100%'}}>
             <Grid item xs={12} md={3}>
                 <Sidebar id={props.id}/>
             </Grid>
 
-            <Grid item x2={12} md={9}>
+            <Grid item xs={12} md={9} style={{order: !isActive ? 0 : -1, height: isActive ? '100vh' : null}}>
                {selectedConversation ? <OpenConversation /> : null}
             </Grid>
         </Grid>
--- src/components/chat/OpenConversation.js
+++ src/components/chat/OpenConversation.js
@@ -10,7 +10,7 @@ import {makeStyles} from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
     list: {
-        width: '90vw',
+        width: '100%',
         display: 'flex',
         flexDirection: 'column'
     },
