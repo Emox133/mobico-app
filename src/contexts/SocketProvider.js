0e91702267e3aa6636e68b85cbdbd5983eb8106e
Emox133 <emir.betaacc0unt@gmail.com>
2020-09-20 09:33:46 +0200

    Socket url fix

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://localhost:8000',
+      'https://cors-anywhere.herokuapp.com/https://mobicoapp.herokuapp.com/api/v1',
       { query: { id } }
     )
     setSocket(newSocket)
