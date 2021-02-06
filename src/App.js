95207d5be241b04f44fbb6bbc841eacbbd96c5af
Emox133 <emir.betaacc0unt@gmail.com>
2021-02-06 13:18:37 +0100

    removed proxy prefix and fixed 403

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import {useDarkMode} from './utils/theme'
 
 import {useSelector, shallowEqual} from 'react-redux'
 
-axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://mobicoapp.herokuapp.com/api/v1'
+axios.defaults.baseURL = 'https://mobicoapp.herokuapp.com/api/v1'
 
 const App = () => {
   const [theme, toggleDarkMode] = useDarkMode();
